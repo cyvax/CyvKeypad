e9c1f275d4df3417b2eeaf345446d1e7173d812e
cyvax <cyvax.123@gmail.com>
2019-11-10 18:04:18 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
     $.getJSON("/data", function(json) {
-        document.getElementById('button1').value = json["button1"];
-        document.getElementById('button2').value = json["button2"];
-        document.getElementById('button3').value = json["button3"];
-        document.getElementById('button4').value = json["button4"];
-        document.getElementById('button5').value = json["button5"];
-        document.getElementById('button6').value = json["button6"];
-        document.getElementById('button7').value = json["button7"];
-        document.getElementById('button8').value = json["button8"];
-        document.getElementById('button9').value = json["button9"];
+        document.getElementById("button1").value = json["button1"];
+        document.getElementById("button2").value = json["button2"];
+        document.getElementById("button3").value = json["button3"];
+        document.getElementById("button4").value = json["button4"];
+        document.getElementById("button5").value = json["button5"];
+        document.getElementById("button6").value = json["button6"];
+        document.getElementById("button7").value = json["button7"];
+        document.getElementById("button8").value = json["button8"];
+        document.getElementById("button9").value = json["button9"];
     });
 });
 
 $(function () {
     $("#button1").click(function (_) {
-        $.get('/button1', {},
+        $.get("/button1", {},
             function (data) {
             });
         return false;
@@ -22,58 +22,56 @@ $(function () {
 });
 $(function() {
     $("#button2").click(function(_) {
-        $.get('/button2', {},
+        $.get("/button2", {},
             function(data) {});
         return false;
     });
 });
 $(function() {
     $("#button3").click(function(_) {
-        $.get('/button3', {},
+        $.get("/button3", {},
             function(data) {});
         return false;
     });
 });
 $(function() {
     $("#button4").click(function(_) {
-        $.get('/button4', {},
+        $.get("/button4", {},
             function(data) {});
         return false;
     });
 });
 $(function() {
     $("#button5").click(function(_) {
-        $.get('/button5', {},
+        $.get("/button5", {},
             function(data) {});
         return false;
     });
 });
 $(function() {
     $("#button6").click(function(_) {
-        $.get('/button6', {},
+        $.get("/button6", {},
             function(data) {});
         return false;
     });
 });
 $(function() {
     $("#button7").click(function(_) {
-        $.get('/button7', {},
+        $.get("/button7", {},
             function(data) {});
         return false;
     });
 });
-
-
 $(function() {
     $("#button8").click(function(_) {
-        $.get('/button8', {},
+        $.get("/button8", {},
             function(data) {});
         return false;
     })
 });
 $(function() {
     $("#button9").click(function(_) {
-        $.get('/button9', {},
+        $.get("/button9", {},
             function(data) {});
         return false;
     });
